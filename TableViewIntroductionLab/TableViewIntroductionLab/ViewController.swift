0e8f9de8aa5d5ff1da2a4871e4384addf0cfc518
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-12 17:48:43 -0500

    private variable task was made

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -10,22 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // outlets
+    // outlets & variables
     @IBOutlet weak var tableView: UITableView!
    
+    private var taskvariable = [Task]() {
+        didSet {
+            tableView.reloadData()
+        }
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
-        
+        //loadData()
     }
-
+/*
+     func loadData{
+     taskVariable = task.getSections
+     }
+     */
+    
 
 }
 
 extension ViewController : UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 0
+       }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -33,11 +50,6 @@ extension ViewController : UITableViewDataSource{
         
         
         return cell
-    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
     }
     
     
