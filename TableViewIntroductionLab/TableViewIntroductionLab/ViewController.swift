7b42d6ec41ba8c9cc02a016a343ced061caa389e
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-12 18:28:55 -0500

    the view is now populated

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
    
      var tasks = [Task]() {
         didSet {
+            
+            
+            // once it gets something(information) it refreshes the view regardless
             tableView.reloadData()
+            
         }
         
     }
@@ -27,8 +31,10 @@ class ViewController: UIViewController {
         tableView.dataSource = self
 //        loadData()
         
-        print("There are \(Task.allTasks.count) tasks in total")
+        print("There are \(Task.allTasks.count) tasks in total") // is empty
         
+        // giving it the information to use here
+        tasks = Task.allTasks
     }
     
     /*
@@ -43,19 +49,27 @@ extension ViewController : UITableViewDataSource{
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // adds how many cell into the story board
         return tasks.count
+        
        }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        // tells what cell to use
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        // tells where the info should go. what it should say
         
+        
+        // what info should be used
         let task = tasks[indexPath.row]
         
-        cell.textLabel?.text = task.name
-        cell.textLabel?.text = "\(task.dueDate)"
-        cell.textLabel?.text = "\(task.status)"
+        
+        //where it goes
+        cell.textLabel?.text = "\(task.name)  \(task.dueDate)"
+        cell.detailTextLabel?.text = "\(task.status)"
         
         return cell
     }
